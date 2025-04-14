e8fd82b2210b0192b3a9cd83ea71d8201744d022
SujayKarpur <PES1UG23CS607@pesu.pes.edu>
2025-04-14 04:53:59 +0000

    Add function declarations

--- src/BinaryTree/RedBlackTree.hs
+++ src/BinaryTree/RedBlackTree.hs
@@ -1,3 +1,25 @@
 data Color = Red | Black 
+data Side = Left | Right 
 
-data Tree a = Empty | Node Color a (Tree a) (Tree a)
+data Tree a = Empty | Node Color a (Tree a) (Tree a) 
+
+contains :: (Ord, Eq a) => Tree a -> a -> Bool 
+contains Empty _ = False 
+contains (Node _ x l r) val 
+                         |x == val = True 
+                         |val > x = contains r val 
+                         |otherwise = contains l val 
+
+
+search :: (Ord, Eq a) => Tree a -> a -> Tree a 
+search Empty _ = Empty 
+search (Node c x l r) val 
+                         |x == val = Node c x l r
+                         |val > x = contains r val 
+                         |otherwise = contains l val 
+
+
+insert :: (Ord, Eq a) => Tree a -> a -> Tree a 
+delete :: (Ord, Eq a) => Tree a -> a -> Tree a 
+display :: (Ord, Eq a) => Tree a -> Nothing 
+rotate :: (Ord, Eq a) => Tree a -> Tree a -> Side -> Tree a  
